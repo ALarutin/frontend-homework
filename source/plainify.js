8863ce4b4b65f59cde7b60b93d891a40b714d9b6
mac <a.larutin@yandex.ru>
2019-03-11 15:26:27 +0300

    Rename function innerFunc - objectSplitting

--- source/plainify.js
+++ source/plainify.js
@@ -1,20 +1,21 @@
 'use strict';
 
+const objectSplitting = (path, obj) => {
+    Object.keys(obj).forEach(key => {
+        const newPath = `${path}${key}`;
+
+        if (Object.keys(obj[key] || {}).length !== 0 && typeof obj[key] === 'object')  {
+            objectSplitting(`${newPath}.`, obj[key]);
+        } else {
+            plainObject[newPath] = obj[key];
+        }
+    });
+};
+
 const plainify = (object) => {
     const plainObject = {};
 
-    const innerFunc = (path, obj) => {
-        Object.keys(obj).forEach(key => {
-            const newPath = `${path}${key}`;
+    objectSplitting('', object);
 
-            if (Object.keys(obj[key] || {}).length !== 0 && typeof obj[key] === 'object')  {
-                innerFunc(`${newPath}.`, obj[key]);
-            } else {
-                plainObject[newPath] = obj[key];
-            }
-        });
-    };
-
-    innerFunc('', object);
     return plainObject;
 };
